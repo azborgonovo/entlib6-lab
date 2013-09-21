e56017a80c71ad1d47a01aebc55135d74dd50233
André <azborgonovo@gmail.com>
2013-09-21 19:20:18 -0300

    Initial

---
+++ EntLib6Lab.Application/Services/IProductsService.cs
@@ -0,0 +1,14 @@
+﻿using EntLib6Lab.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntLib6Lab.Application.Services
+{
+    public interface IProductsService
+    {
+        void AddProduct(ProductDto product);
+    }
+}
---
+++ EntLib6Lab.Application/Services/ProductsService.cs
@@ -0,0 +1,31 @@
+﻿using EntLib6Lab.Application.Dtos;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntLib6Lab.Application.Services
+{
+    public class ProductsService : IProductsService
+    {
+        ExceptionManager _exceptionManager;
+
+        public ProductsService(ExceptionManager exceptionManager)
+        {
+            if (exceptionManager == null) throw new ArgumentNullException("exceptionManager");
+
+            _exceptionManager = exceptionManager;
+        }
+
+        public void AddProduct(ProductDto product)
+        {
+            _exceptionManager.Process(() =>
+            {
+                Console.WriteLine(string.Format("The product {0} has been added!", product.Name));
+
+            }, "ShieldingException");
+        }
+    }
+}
